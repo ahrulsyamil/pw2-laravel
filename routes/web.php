42a5f36cf3cc69a565e2d83754b37aa7640ea6de
ahrulsyamil <ahrulsyamilulum@gmail.com>
2023-05-12 00:08:06 +0700

    feat: tugas pertemuan 8

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,12 +17,20 @@ use App\Http\Controllers\SiswaController;
 */
 
 Route::get('/', function () {
-    return view('dashboard');
+    return redirect('dashboard');
 });
 
-Route::get('/siswa', [SiswaController::class, 'index']);
-Route::post('/siswa/create', [SiswaController::class, 'create']);
-Route::get('/siswa/{id}/edit', [SiswaController::class, 'edit']);
-Route::post('/siswa/{id}/update', [SiswaController::class, 'update']);
-Route::get('/siswa/{id}/delete', [SiswaController::class, 'delete']);
-Route::get('/siswa/{id}/profile', [SiswaController::class, 'profile']);
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/postlogin', [AuthController::class, 'postLogin']);
+Route::get('/logout', [AuthController::class, 'logout']);
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+
+    Route::get('/siswa', [SiswaController::class, 'index']);
+    Route::post('/siswa/create', [SiswaController::class, 'create']);
+    Route::get('/siswa/{id}/edit', [SiswaController::class, 'edit']);
+    Route::post('/siswa/{id}/update', [SiswaController::class, 'update']);
+    Route::get('/siswa/{id}/delete', [SiswaController::class, 'delete']);
+    Route::get('/siswa/{id}/profile', [SiswaController::class, 'profile']);
+});
