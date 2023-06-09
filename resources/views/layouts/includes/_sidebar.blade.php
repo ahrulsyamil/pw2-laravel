42859df0d076c65ec27f4c3c982112397dde062e
ahrulsyami <ahrulsyamilulum@gmail.com>
2023-06-09 15:09:02 +0700

    feat: tugas pertemuan 12

--- resources/views/layouts/includes/_sidebar.blade.php
+++ resources/views/layouts/includes/_sidebar.blade.php
@@ -1,12 +1,14 @@
 <div id="sidebar-nav" class="sidebar">
-   <div class="sidebar-scroll">
-      <nav>
-         <ul class="nav">
-            <li><a href="/" class="active"><i class="lnr lnr-home"></i>
-                  <span>Dashboard</span></a></li>
-            <li><a href="/siswa" class=""><i class="lnr lnr-user"></i>
-                  <span>Siswa</span></a></li>
-         </ul>
-      </nav>
-   </div>
+    <div class="sidebar-scroll">
+        <nav>
+            <ul class="nav">
+                <li><a href="/" class="active"><i class="lnr lnr-home"></i>
+                        <span>Dashboard</span></a></li>
+                <li><a href="/siswa" class=""><i class="lnr lnr-user"></i>
+                        <span>Siswa</span></a></li>
+                <li><a href="/mat-pel" class=""><i class="lnr lnr-book"></i> <span>Mata
+                            Pelajaran</span></a></li>
+            </ul>
+        </nav>
+    </div>
 </div>
